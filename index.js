d3af8f262a08bd813bb79eb5af777fbb4fb72129
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-19 15:31:35 -0600

    made .post and tested in postman and San Diego Zoo successfully added

--- index.js
+++ index.js
@@ -30,6 +30,39 @@ server.get('/api/zoos', async (req, res) => {
   }
 });
 
+// list a zoo by id
+server.get('/api/zoos/:id', async (req, res) => {
+  // get the zoos from the database
+  try {
+    const zoo = await db('zoos')
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(zoo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+const errors = {
+  '19': 'Another record with that value exists',
+};
+
+// create zoos
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const [id] = await db('zoos').insert(req.body);
+
+    const role = await db('zoos')
+      .where({ id })
+      .first();
+
+    res.status(201).json(zoo);
+  } catch (error) {
+    const message = errors[error.errno] || 'We ran into an error';
+    res.status(500).json({ message, error });
+  }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
