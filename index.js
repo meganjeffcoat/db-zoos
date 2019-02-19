b3e23231bdcf0ee277dc32872b627048b895583a
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-19 14:32:29 -0600

    knexConfig complete

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3',
+  },
+  useNullAsDefault: true, // needed for sqlite
+};
+const db = knex(knexConfig);
+
 
 const server = express();
 
