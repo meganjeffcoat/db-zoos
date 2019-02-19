acd200bb5ec4946e544e43ab11612ae1641f59dd
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-19 14:42:19 -0600

    .get ran for /api/zoos all and received empty array

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ server.use(helmet());
 
 // endpoints here
 
+// list all zoos
+server.get('/api/zoos', async (req, res) => {
+  // get the zoos from the database
+  try {
+    const zoos = await db('zoos'); // all the records from the table
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
